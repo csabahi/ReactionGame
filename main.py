afb0d7bd7d04c54a572393cf48c34d634719cbd2
Camron Sabahi <sabahi.camron@gmail.com>
2023-09-18 18:57:08 -0400

    hand capture working

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 import cv2
+from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)  # Use 0 for the default camera
 
 # Set the frame width and height
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
+detector = HandDetector(detectionCon=0.6, maxHands=2)
+
 while True:
-    ret, frame = cap.read()
+    frame = cap.read()[1]  # Read the frame directly, without using ret
 
-    if not ret:
-        print('Error: Could not read frame.')
-        break
+    hands, frame = detector.findHands(frame, flipType=False)
+    if hands:
+        print(f"Detected {len(hands)} hand(s)")
 
-    cv2.imshow("Reaction Game", frame)  
-    
-    k = cv2.waitKey(1) & 0xFF 
+    cv2.imshow("Reaction Game", frame)
+
+    k = cv2.waitKey(1) & 0xFF
     if k == ord('q'):
         break
 
-from cvzone.HandTrackingModule import HandDetector
-
-detector = HandDetector(detectionCon=0.8, maxHands=2)
-
-while True:
-    frame = cap.read()
-    hands, frame = detector.findHands(frame, flipType=False)
-    cv2.imshow("Reaction Game", frame)
-
+cap.release()
+cv2.destroyAllWindows()
