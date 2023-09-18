28c0f2b93464683f81786c3e422173e5dad62349
Camron Sabahi <sabahi.camron@gmail.com>
2023-09-18 18:24:58 -0400

    Fixed video capture error (mac)

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import cv2
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0)  # Use 0 for the default camera
 
 # Set the frame width and height
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 while True:
-    ret, frame = cap.read() 
-    
+    ret, frame = cap.read()
+
+    if not ret:
+        print('Error: Could not read frame.')
+        break
+
     cv2.imshow("Reaction Game", frame)  
     
     k = cv2.waitKey(1) & 0xFF 
