9908edeb7ffda877e5c75cda70a7cb0896d1be42
Camron Sabahi <sabahi.camron@gmail.com>
2023-09-18 18:13:12 -0400

    fixed cap issues

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import cv2
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+
+# Set the frame width and height
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 while True:
-    ret, frame = cap.read()
-
-    cv2.imshow("Reaction Game", frame)
-
-    k = cv2.waitKey(1) & 0xFF
+    ret, frame = cap.read() 
+    
+    cv2.imshow("Reaction Game", frame)  
+    
+    k = cv2.waitKey(1) & 0xFF 
     if k == ord('q'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
