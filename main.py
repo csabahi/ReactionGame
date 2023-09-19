8e162e18b120b5c758f61da9cfcd0f47b41aa706
Camron Sabahi <sabahi.camron@gmail.com>
2023-09-18 20:51:21 -0400

    writing data to file

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pickle
 import time
 from cvzone.HandTrackingModule import HandDetector
 
-width = 500
-height = 500
+width = 1080
+height = 760
 hand_radius = 40
 target_radius = 40  
 border_size = 15  
@@ -31,51 +31,18 @@ class Circle:
     def check_intersections(self, other_coordinates, other_radius): 
 
         distance = math.sqrt(math.pow(other_coordinates[0] - self.coordinates[0], 2) 
-                             + math.pow(other_coordinates[1] - self.coordinates[1], 2))
+                    + math.pow(other_coordinates[1] - self.coordinates[1], 2))
         if distance <= self.radius + other_radius: 
             return True
         else: 
             return False  
 
-# def create_random_target(current_target_pos=[]):
-#     if current_target_pos:
-#         possible_x = []
-
-#         x_limit = [target_radius + border_size + 15, width - target_radius - border_size - 15]
-#         y_limit = [target_radius + border_size + 15, height - target_radius - border_size - 15]
-
-#         for i in range(x_limit[0], x_limit[1]):
-#             if i + 100 < current_target_pos[0] or i - 100 > current_target_pos[0]:
-#                 possible_x.append(i)
-
-#         possible_y = []
-#         for i in range(y_limit[0], y_limit[1]):
-#             if i + 100 < current_target_pos[1] or i - 100 > current_target_pos[1]:
-#                 possible_y.append(i)
-
-#         if not possible_x:
-#             possible_x = range(x_limit[0], x_limit[1])
-
-#         if not possible_y:
-#             possible_y = range(y_limit[0], y_limit[1])
-
-#     else:
-#         possible_x = range(target_radius + border_size, width - target_radius - border_size)
-#         possible_y = range(target_radius + border_size, height - target_radius - border_size)
-
-#     random_x = random.choice(possible_x)
-#     random_y = random.choice(possible_y)
-#     random_color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 256)]
-#     _target = Circle([random_x, random_y], target_radius, random_color, -1)
-
-#     return random_x, random_y
-
 def create_random_target(current_target_pos=[]):
     if current_target_pos:
         possible_x = []
 
-        x_limit = [target_radius + border_size + 15, width - target_radius - border_size - 15]
-        y_limit = [target_radius + border_size + 15, height - target_radius - border_size - 15]
+        x_limit = [target_radius + border_size + 20, width - target_radius - border_size - 15]
+        y_limit = [target_radius + border_size + 20, height - target_radius - border_size - 15]
 
         for i in range(x_limit[0], x_limit[1]):
             if i + 200 < current_target_pos[0] or i - 200 > current_target_pos[0]:
@@ -109,12 +76,11 @@ def create_random_target(current_target_pos=[]):
 target = create_random_target()
 
 cap = cv2.VideoCapture(0)  # Use 0 for the default camera
-
 # Set the frame width and height
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-detector = HandDetector(detectionCon=0.6, maxHands=2)
+detector = HandDetector(detectionCon=0.2, maxHands=2)
 target = Circle((int(width/2), int(height/2)), 50, (0, 0, 255), -1)
 
 
@@ -124,6 +90,7 @@ while True:
     frame = cap.read()[1]  # Read the frame directly, without using ret
 
     hands, frame = detector.findHands(frame, flipType=True)
+    # frame = cv2.flip(frame, 1)
 
     cv2.imshow("Reaction Game", frame)
 
@@ -134,7 +101,7 @@ while True:
     if hands:
         for i in range(len(hands)):
             hand_position = hands[i]["center"]
-            hand_circle = Circle(hand_position, hand_radius, (0, 0, 255), 1)
+            hand_circle = Circle(hand_position, hand_radius, (0, 0, 0), -1)
             
             if target.check_intersections(hand_circle.coordinates, hand_circle.radius): 
                 hit_target = True
@@ -151,6 +118,11 @@ while True:
 
     if hit_target:
         target_count += 1
+        if target_count == 5:
+            with open('output.txt', 'w') as file: 
+                for time in time_array: 
+                    file.write(str(time) + '\n')
+          
         # print("count =", target_count)
         target = create_random_target(target.coordinates)
         print(target.coordinates)
